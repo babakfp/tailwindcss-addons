6334d58527acb1a10d0479694ee99e6cf563bdf2
babakfp <44144724+babakfp@users.noreply.github.com>
2022-09-06 13:21:46 -0700

    Renamed files

--- tailwindcss-addons/index.cjs
+++ tailwindcss-addons/index.cjs
@@ -2,10 +2,10 @@
 const dir = require('./src/utilities/dir.cjs')
 const drag = require('./src/utilities/drag.cjs')
 const flip = require('./src/utilities/flip.cjs')
-const insetCenter = require('./src/utilities/inset-center.cjs')
-const minMaxWidthMinMaxHeightMergedWithSpacing = require('./src/utilities/min-max-width-min-max-height-merged-with-spacing.cjs')
-const overflowUnset = require('./src/utilities/overflow-unset.cjs')
-const shortPlacements = require('./src/utilities/short-placements.cjs')
+const insetCenter = require('./src/utilities/insetCenter.cjs')
+const minMaxWidthMinMaxHeightMergedWithSpacing = require('./src/utilities/minMaxWidthMinMaxHeightMergedWithSpacing.cjs')
+const overflowUnset = require('./src/utilities/overflowUnset.cjs')
+const shortPlacements = require('./src/utilities/shortPlacements.cjs')
 
 // Components
 const linkImg = require('./src/components/link-img.cjs')
