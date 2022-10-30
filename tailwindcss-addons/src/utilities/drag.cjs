acf5caaac3590e45986437c1e85805705c0d9f6e
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-30 03:00:59 -0700

    fixed drag prefix

--- tailwindcss-addons/src/utilities/drag.cjs
+++ tailwindcss-addons/src/utilities/drag.cjs
@@ -3,13 +3,13 @@ const plugin = require('tailwindcss/plugin')
 module.exports = plugin(({ addUtilities }) => {
 	addUtilities({
 		'.drag-none': {
-			'user-drag': 'none',
+			'-webkit-user-drag': 'none',
 		},
 		'.drag-element': {
-			'user-drag': 'element',
+			'-webkit-user-drag': 'element',
 		},
 		'.drag-auto': {
-			'user-drag': 'auto',
+			'-webkit-user-drag': 'auto',
 		},
 	})
 })
