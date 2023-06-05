3ec583eb7c3b43b6757703aa542a6631cdbdee19
Babak Farkhoopak <babak.bxf@gmail.com>
2023-06-05 08:44:05 -0700

    Added Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    arrowParens: "avoid",
+    overrides: [
+        { files: "*.svelte", options: { parser: "svelte" } },
+        { files: "*.md", options: { tabWidth: 2 } },
+    ],
+    plugins: [
+        require("prettier-plugin-svelte"),
+    ],
+}
