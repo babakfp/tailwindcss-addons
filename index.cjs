79cb1e749d182072bd19c0e2d21d81f3cfed9a55
Babak Farkhoopak <babak.bxf@gmail.com>
2023-06-05 20:12:08 -0700

    Cleaner code

--- index.cjs
+++ index.cjs
@@ -1,5 +1,21 @@
 const deepMerge = require("./src/lib/deepMerge.cjs")
 
+const extendedValues = require("./src/extendedValues.cjs")
+const dynamicViewFix = require("./src/dynamicViewFix.cjs")
+const fontWeightRegular = require("./src/fontWeightRegular.cjs")
+const bgGrid = require("./src/bgGrid.cjs")
+const bgRadial = require("./src/bgRadial.cjs")
+const dir = require("./src/dir.cjs")
+const drag = require("./src/drag.cjs")
+const flexGrid = require("./src/flexGrid.cjs")
+const flip = require("./src/flip.cjs")
+const hideShow = require("./src/hideShow.cjs")
+const inputResets = require("./src/inputResets.cjs")
+const insetCenter = require("./src/insetCenter.cjs")
+const overflowUnset = require("./src/overflowUnset.cjs")
+const tapHighlight = require("./src/tapHighlight.cjs")
+const notVariants = require("./src/notVariants.cjs")
+
 const defaultConfig = {
     extendedValues: true,
     dynamicViewFix: false,
@@ -27,29 +43,26 @@ const tailwindcssAddons = (userConfig = {}) => {
 
     return {
         presets: [
-            config.extendedValues && require("./src/extendedValues.cjs"),
-            config.dynamicViewFix && require("./src/dynamicViewFix.cjs"),
+            config.extendedValues && extendedValues,
+            config.dynamicViewFix && dynamicViewFix,
         ],
         theme: {
-            ...(config.utilities.fontWeightRegular
-                ? require("./src/fontWeightRegular.cjs")
-                : {}),
+            ...(config.utilities.fontWeightRegular ? fontWeightRegular : {}),
         },
         plugins: [
-            config.utilities.bgGrid && require("./src/bgGrid.cjs"),
-            config.utilities.bgRadial && require("./src/bgRadial.cjs"),
-            config.utilities.dir && require("./src/dir.cjs"),
-            config.utilities.drag && require("./src/drag.cjs"),
-            config.utilities.flexGrid && require("./src/flexGrid.cjs"),
-            config.utilities.flip && require("./src/flip.cjs"),
-            config.utilities.hideShow && require("./src/hideShow.cjs"),
-            config.utilities.inputResets && require("./src/inputResets.cjs"),
-            config.utilities.insetCenter && require("./src/insetCenter.cjs"),
-            config.utilities.overflowUnset &&
-                require("./src/overflowUnset.cjs"),
-            config.utilities.tapHighlight && require("./src/tapHighlight.cjs"),
+            config.utilities.bgGrid && bgGrid,
+            config.utilities.bgRadial && bgRadial,
+            config.utilities.dir && dir,
+            config.utilities.drag && drag,
+            config.utilities.flexGrid && flexGrid,
+            config.utilities.flip && flip,
+            config.utilities.hideShow && hideShow,
+            config.utilities.inputResets && inputResets,
+            config.utilities.insetCenter && insetCenter,
+            config.utilities.overflowUnset && overflowUnset,
+            config.utilities.tapHighlight && tapHighlight,
 
-            config.variants.notVariants && require("./src/notVariants.cjs"),
+            config.variants.notVariants && notVariants,
         ],
         corePlugins: {
             ...(config.utilities.flexGrid
