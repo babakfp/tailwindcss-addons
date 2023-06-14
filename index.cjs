b4d2d27f1766b376a3883bc6bd2f9337d9c4ebd2
babakfp <babak.bxf@gmail.com>
2023-06-14 09:43:57 -0700

    Formatted

--- index.cjs
+++ index.cjs
@@ -1,12 +1,12 @@
 const deepMerge = require("./src/lib/deepMerge.cjs")
 
 const defaultConfig = {
-	presets: {
+    presets: {
         flexGrid: false,
-		fontWeightRegular: false,
-		moreDefaultValues: true,
-		screenSizeFix: false,
-	},
+        fontWeightRegular: false,
+        moreDefaultValues: true,
+        screenSizeFix: false,
+    },
     utilities: {
         bgGrid: true,
         bgRadial: true,
@@ -29,24 +29,34 @@ module.exports = (userConfig = {}) => {
 
     return {
         presets: [
-			config.presets.flexGrid && require("./src/presets/flexGrid.cjs"),
-			config.presets.fontWeightRegular && require("./src/presets/fontWeightRegular.cjs"),
-			config.presets.moreDefaultValues && require("./src/presets/moreDefaultValues.cjs"),
-            config.presets.screenSizeFix && require("./src/presets/screenSizeFix.cjs"),
+            config.presets.flexGrid && require("./src/presets/flexGrid.cjs"),
+            config.presets.fontWeightRegular &&
+                require("./src/presets/fontWeightRegular.cjs"),
+            config.presets.moreDefaultValues &&
+                require("./src/presets/moreDefaultValues.cjs"),
+            config.presets.screenSizeFix &&
+                require("./src/presets/screenSizeFix.cjs"),
         ],
         plugins: [
             config.utilities.bgGrid && require("./src/utilities/bgGrid.cjs"),
-            config.utilities.bgRadial && require("./src/utilities/bgRadial.cjs"),
+            config.utilities.bgRadial &&
+                require("./src/utilities/bgRadial.cjs"),
             config.utilities.dir && require("./src/utilities/dir.cjs"),
             config.utilities.drag && require("./src/utilities/drag.cjs"),
             config.utilities.flip && require("./src/utilities/flip.cjs"),
-            config.utilities.hideShow && require("./src/utilities/hideShow.cjs"),
-            config.utilities.inputResets && require("./src/utilities/inputResets.cjs"),
-            config.utilities.insetCenter && require("./src/utilities/insetCenter.cjs"),
-            config.utilities.overflowUnset && require("./src/utilities/overflowUnset.cjs"),
-            config.utilities.tapHighlight && require("./src/utilities/tapHighlight.cjs"),
+            config.utilities.hideShow &&
+                require("./src/utilities/hideShow.cjs"),
+            config.utilities.inputResets &&
+                require("./src/utilities/inputResets.cjs"),
+            config.utilities.insetCenter &&
+                require("./src/utilities/insetCenter.cjs"),
+            config.utilities.overflowUnset &&
+                require("./src/utilities/overflowUnset.cjs"),
+            config.utilities.tapHighlight &&
+                require("./src/utilities/tapHighlight.cjs"),
 
-            config.variants.notVariants && require("./src/variants/notVariants.cjs"),
+            config.variants.notVariants &&
+                require("./src/variants/notVariants.cjs"),
         ],
     }
 }
--- src/presets/fontWeightRegular.cjs
+++ src/presets/fontWeightRegular.cjs
@@ -1,15 +1,15 @@
 module.exports = () => ({
     theme: {
-		fontWeight: {
-			thin: "100",
-			extralight: "200",
-			light: "300",
-			regular: "400", // Replacing `normal` with `regular`
-			medium: "500",
-			semibold: "600",
-			bold: "700",
-			extrabold: "800",
-			black: "900",
-		},
+        fontWeight: {
+            thin: "100",
+            extralight: "200",
+            light: "300",
+            regular: "400", // Replacing `normal` with `regular`
+            medium: "500",
+            semibold: "600",
+            bold: "700",
+            extrabold: "800",
+            black: "900",
+        },
     },
 })
