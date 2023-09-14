aa7793b8f689e1b93e257fde9a24b4a263f56976
babakfp <babak.bxf@gmail.com>
2023-09-13 20:27:12 -0700

    Formatted

--- src/index.js
+++ src/index.js
@@ -31,12 +31,9 @@ export default (userConfig = {}) => {
         presets: [
             config.presets.flexGrid && import("./presets/flexGrid.js"),
             // TODO: In TailwindCSS v3.3.2 there is a bug: https://github.com/tailwindlabs/tailwindcss/issues/11428
-            config.presets.fontWeightRegular &&
-                import("./presets/fontWeightRegular.js"),
-            config.presets.moreDefaultValues &&
-                import("./presets/moreDefaultValues.js"),
-            config.presets.screenSizeFix &&
-                import("./presets/screenSizeFix.js"),
+            config.presets.fontWeightRegular && import("./presets/fontWeightRegular.js"),
+            config.presets.moreDefaultValues && import("./presets/moreDefaultValues.js"),
+            config.presets.screenSizeFix && import("./presets/screenSizeFix.js"),
         ],
         plugins: [
             config.utilities.bgGrid && import("./utilities/bgGrid.js"),
@@ -45,14 +42,10 @@ export default (userConfig = {}) => {
             config.utilities.drag && import("./utilities/drag.js"),
             config.utilities.flip && import("./utilities/flip.js"),
             config.utilities.hideShow && import("./utilities/hideShow.js"),
-            config.utilities.inputResets &&
-                import("./utilities/inputResets.js"),
-            config.utilities.insetCenter &&
-                import("./utilities/insetCenter.js"),
-            config.utilities.overflowUnset &&
-                import("./utilities/overflowUnset.js"),
-            config.utilities.tapHighlight &&
-                import("./utilities/tapHighlight.js"),
+            config.utilities.inputResets && import("./utilities/inputResets.js"),
+            config.utilities.insetCenter && import("./utilities/insetCenter.js"),
+            config.utilities.overflowUnset && import("./utilities/overflowUnset.js"),
+            config.utilities.tapHighlight && import("./utilities/tapHighlight.js"),
 
             config.variants.notVariants && import("./variants/notVariants.js"),
         ],
