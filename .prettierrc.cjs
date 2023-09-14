897aebba53439b8c61887c096e9eb7618860f04e
babakfp <babak.bxf@gmail.com>
2023-09-13 17:41:51 -0700

    Removed non relevant packages and configs

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,6 +2,4 @@ module.exports = {
     semi: false,
     tabWidth: 4,
     arrowParens: "avoid",
-    overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
-    plugins: [require("prettier-plugin-svelte")],
 }
