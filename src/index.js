9c290130382961ba1479c5ad781020c922c04362
babakfp <babak.bxf@gmail.com>
2023-09-13 22:21:09 -0700

    Fixed promise

--- src/index.js
+++ src/index.js
@@ -29,25 +29,25 @@ export default (userConfig = {}) => {
 
     return {
         presets: [
-            config.presets.flexGrid && import("./presets/flexGrid.js"),
+            (async () => config.presets.flexGrid && await import("./presets/flexGrid.js"))(),
             // TODO: In TailwindCSS v3.3.2 there is a bug: https://github.com/tailwindlabs/tailwindcss/issues/11428
-            config.presets.fontWeightRegular && import("./presets/fontWeightRegular.js"),
-            config.presets.moreDefaultValues && import("./presets/moreDefaultValues.js"),
-            config.presets.screenSizeFix && import("./presets/screenSizeFix.js"),
+            (async () => config.presets.fontWeightRegular && await import("./presets/fontWeightRegular.js"))(),
+            (async () => config.presets.moreDefaultValues && await import("./presets/moreDefaultValues.js"))(),
+            (async () => config.presets.screenSizeFix && await import("./presets/screenSizeFix.js"))(),
         ],
         plugins: [
-            config.utilities.bgGrid && import("./utilities/bgGrid.js"),
-            config.utilities.bgRadial && import("./utilities/bgRadial.js"),
-            config.utilities.dir && import("./utilities/dir.js"),
-            config.utilities.drag && import("./utilities/drag.js"),
-            config.utilities.flip && import("./utilities/flip.js"),
-            config.utilities.hideShow && import("./utilities/hideShow.js"),
-            config.utilities.inputResets && import("./utilities/inputResets.js"),
-            config.utilities.insetCenter && import("./utilities/insetCenter.js"),
-            config.utilities.overflowUnset && import("./utilities/overflowUnset.js"),
-            config.utilities.tapHighlight && import("./utilities/tapHighlight.js"),
+            (async () => config.utilities.bgGrid && await import("./utilities/bgGrid.js"))(),
+            (async () => config.utilities.bgRadial && await import("./utilities/bgRadial.js"))(),
+            (async () => config.utilities.dir && await import("./utilities/dir.js"))(),
+            (async () => config.utilities.drag && await import("./utilities/drag.js"))(),
+            (async () => config.utilities.flip && await import("./utilities/flip.js"))(),
+            (async () => config.utilities.hideShow && await import("./utilities/hideShow.js"))(),
+            (async () => config.utilities.inputResets && await import("./utilities/inputResets.js"))(),
+            (async () => config.utilities.insetCenter && await import("./utilities/insetCenter.js"))(),
+            (async () => config.utilities.overflowUnset && await import("./utilities/overflowUnset.js"))(),
+            (async () => config.utilities.tapHighlight && await import("./utilities/tapHighlight.js"))(),
 
-            config.variants.notVariants && import("./variants/notVariants.js"),
+            (async () => config.variants.notVariants && await import("./variants/notVariants.js"))(),
         ],
     }
 }
