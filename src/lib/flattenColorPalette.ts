3a9b070f713c50706730d0d3d774c865acdec294
babakfp <babak.bxf@gmail.com>
2024-09-01 15:24:57 +0330

    Renamed

--- src/lib/flattenColorPalette.ts
+++ src/lib/flattenColorPalette.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error
-import _flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette.js"
+import flattenColorPalette_ from "tailwindcss/lib/util/flattenColorPalette.js"
 
 interface RecursiveStringRecord {
     [key: string]: string | RecursiveStringRecord
@@ -8,5 +8,5 @@ interface RecursiveStringRecord {
 export const flattenColorPalette = (
     colors: RecursiveStringRecord,
 ): Record<string, string> => {
-    return _flattenColorPalette(colors)
+    return flattenColorPalette_(colors)
 }
