6f3b53effa5de328cfe487a1a6d1b55db7756f26
babakfp <babak.bxf@gmail.com>
2024-09-12 20:43:59 +0330

    Better hocus variants

--- src/plugins/hocus.ts
+++ src/plugins/hocus.ts
@@ -2,7 +2,27 @@ import plugin from "tailwindcss/plugin.js"
 
 /** [Documenation](https://babakfp.ir/docs/tailwindcss-addons/hocus) */
 export default plugin(({ addVariant }) => {
-    addVariant("hocus", ["&:hover", "&:focus"])
     addVariant("hocus-within", ["&:hover", "&:focus-within"])
+    addVariant("hocus", ["&:hover", "&:focus"])
     addVariant("hocus-visible", ["&:hover", "&:focus-visible"])
+
+    addVariant("peer-hocus-within", [
+        ".peer:hover ~ &",
+        ".peer:focus-within ~ &",
+    ])
+    addVariant("peer-hocus", [".peer:hover ~ &", ".peer:focus ~ &"])
+    addVariant("peer-hocus-visible", [
+        ".peer:hover ~ &",
+        ".peer:focus-visible ~ &",
+    ])
+
+    addVariant("group-hocus-within", [
+        ".group:hover &",
+        ".group:focus-within &",
+    ])
+    addVariant("group-hocus", [".group:hover &", ".group:focus &"])
+    addVariant("group-hocus-visible", [
+        ".group:hover &",
+        ".group:focus-visible &",
+    ])
 })
--- src/plugins/supportsHocus.ts
+++ src/plugins/supportsHocus.ts
@@ -2,11 +2,11 @@ import plugin from "tailwindcss/plugin.js"
 
 /** [Documenation](https://babakfp.ir/docs/tailwindcss-addons/supports-hocus) */
 export default plugin(({ addVariant }) => {
-    addVariant("supports-hocus", "@media (hover: hover) and (focus: focus)")
     addVariant(
         "supports-hocus-within",
         "@media (hover: hover) and (focus-within: focus-within)",
     )
+    addVariant("supports-hocus", "@media (hover: hover) and (focus: focus)")
     addVariant(
         "supports-hocus-visible",
         "@media (hover: hover) and (focus-visible: focus-visible)",
