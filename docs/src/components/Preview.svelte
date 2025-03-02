127c99f911509551766281c09fb43861a82dbb32
babakfp <babak.bxf@gmail.com>
2025-03-03 03:24:44 +0330

    docs: fix shadow color

--- docs/src/components/Preview.svelte
+++ docs/src/components/Preview.svelte
@@ -15,7 +15,7 @@
 
 <div
     class={twMerge(
-        `rounded border-2 border-gray-800 p-4 ${useGrid ? "bg-grid-gray-800/50 bg-center shadow-[inset_0_0_4rem_2rem_var(--color-gray-900)]" : ""}`,
+        `rounded border-2 border-gray-800 p-4 ${useGrid ? "bg-grid-gray-800/50 bg-center shadow-[inset_0_0_4rem_2rem_var(--sl-color-black)]" : ""}`,
         class_,
     )}
 >
