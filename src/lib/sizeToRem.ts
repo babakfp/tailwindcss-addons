963daa7ee0eec8f704a17aafffb47b9562ae396d
babakfp <babak.bxf@gmail.com>
2025-03-02 06:38:23 +0330

    feat: migrate to tailwind 4

--- src/lib/sizeToRem.ts
+++
@@ -1,2 +0,0 @@
-export const sizeToRem = (sizes: number[]) =>
-    Object.fromEntries(sizes.map((size) => [size, `${size / 4}rem`]))
--- src/plugins/supportsHover.ts
+++
@@ -1,6 +0,0 @@
-import plugin from "tailwindcss/plugin.js"
-
-/** [Documenation](https://babakfp.ir/docs/tailwindcss-addons/supports-hover) */
-export default plugin(({ addVariant }) => {
-    addVariant("supports-hover", "@media (hover: hover)")
-})
