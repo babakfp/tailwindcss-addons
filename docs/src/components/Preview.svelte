f3d5b30b83fd55df4c89408898eddf23e364ee4e
babakfp <babak.bxf@gmail.com>
2025-03-03 04:31:18 +0330

    docs: remove unneeded dep

--- docs/src/components/Preview.svelte
+++ docs/src/components/Preview.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import type { Snippet } from "svelte"
-    import { twMerge } from "tailwind-merge"
 
     let {
         class: class_,
@@ -14,10 +13,9 @@
 </script>
 
 <div
-    class={twMerge(
-        `rounded border-2 border-gray-800 p-4 ${useGrid ? "bg-grid-gray-800/50 bg-center shadow-[inset_0_0_4rem_2rem_var(--sl-color-black)]" : ""}`,
-        class_,
-    )}
+    class={
+        `rounded border-2 border-gray-800 p-4 ${useGrid ? "bg-grid-gray-800/50 bg-center shadow-[inset_0_0_4rem_2rem_var(--sl-color-black)]" : ""}`
+    }
 >
     {@render children()}
 </div>
