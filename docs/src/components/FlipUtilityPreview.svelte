33a329e47894a9063f85984e5bf3f76b18ec7dd9
babakfp <babak.bxf@gmail.com>
2025-03-03 03:45:02 +0330

    docs: fix styles

--- docs/src/components/FlipUtilityPreview.svelte
+++ docs/src/components/FlipUtilityPreview.svelte
@@ -4,20 +4,20 @@
     let { utilities }: { utilities: [string, string][] } = $props()
 </script>
 
-<Preview class="p-8">
+<Preview >
     <div class="flex flex-wrap gap-8">
-        <div class="grid flex-1 justify-items-center gap-4">
-            <!-- TODO: remove ...? -->
-            <span class="font-mono text-gray-400">...</span>
-            <div class="size-8 border-t-2 border-l-2 border-gray-200"></div>
-        </div>
+        {@render Item({ utility: "ORIGINAL" })}
         {#each utilities as [utility]}
-            <div class="grid flex-1 justify-items-center gap-4">
-                <span class="font-mono text-gray-400">.{utility}</span>
-                <div
-                    class="size-8 border-t-2 border-l-2 border-gray-200 {utility}"
-                ></div>
-            </div>
+            {@render Item({ utility, className: "font-mono" })}
         {/each}
     </div>
 </Preview>
+
+{#snippet Item({ utility, className }: { utility: string; className?: string })}
+    <div class="flex mt-0! flex-col flex-1 gap-2 justify-center text-nowrap p-8 items-center">
+        <span class="text-gray-400 text-sm {className}">{utility}</span>
+        <div
+            class="size-8 border-4 border-b-(--sl-color-green) border-r-(--sl-color-green) border-gray-200 {utility}" 
+        ></div>
+    </div>
+{/snippet}
